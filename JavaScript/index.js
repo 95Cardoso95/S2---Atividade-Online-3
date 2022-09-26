33faa2299280a63d13c4d4c8a37e583d6103153b
Cardoso <lucas95felipe@gmail.com>
2022-09-26 20:05:02 -0300

    jquery

---
+++ JavaScript/index.js
@@ -0,0 +1,35 @@
+alert( "Cadastre-se no nosso site para ter acesso as nossas promoções")
+
+//Jquerry//
+$('#img1').mouseenter(
+    function(){
+      $('#img1').animate({width:'300px',})
+    }
+  )
+  $('#img1').mouseleave(
+    function(){
+      $('#img1').animate({width:'20%'})
+    }
+  )
+
+  $('#img2').mouseenter(
+    function(){
+      $('#img2').animate({width:'300px'})
+    }
+  )
+  $('#img2').mouseleave(
+    function(){
+      $('#img2').animate({width:'20%'})
+    }
+  )
+
+  $('#img3').mouseenter(
+    function(){
+      $('#img3').animate({width:'300px'})
+    }
+  )
+  $('#img3').mouseleave(
+    function(){
+      $('#img3').animate({width:'20%'})
+    }
+  )
